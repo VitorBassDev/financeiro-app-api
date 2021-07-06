23128e481d5dbfc57f688d64e2a8f82ce40533c7
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-06 10:55:13 -0300

    Style: Reestruturaçao do Projeto ReactJs Remoçao de arquivos nao utilizados Ajuste da pagina inicial

---
+++ app-financeiro/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Controle Financeiro | App-ReactJS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
