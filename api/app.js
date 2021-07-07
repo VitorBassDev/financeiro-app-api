10fe7a2a9c03d105f39b7fe68f07b5810bf079ef
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-07 17:51:14 -0300

    Feat: Cadastro de Lancamentos Cadastra os lancamentos no banco de dados

---
+++ api/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const { Op } = require('sequelize');
+
+const Lancamentos = require('./database/models/Lancamentos');
+
+app.use(express.json());
+
+app.post('/cadastrar', async (req, res) => {
+    // console.log(req.body)
+    await Lancamentos.create(req.body).then(function(){
+        return res.json({
+            erro: false,
+            mensagem: "Lançamento cadastrado com sucesso!"
+        });
+        
+    }).catch(function(){
+        // console.log(req.body)
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Lançamento não cadastrado!"
+        });
+    });
+});
+
+
+app.listen(8080, function(){
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
