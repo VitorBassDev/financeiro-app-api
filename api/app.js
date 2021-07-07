cbe906d99ae1498a6bb68874d1b87870c9ca2232
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-07 18:06:20 -0300

    Feat: Filtro de Lista Listar por mes e por ano

--- api/app.js
+++ api/app.js
@@ -42,6 +42,59 @@ app.get('/listar', async (req, res) => {
     });
 });
 
+app.get('/listar/:mes/:ano', async (req, res) => {
+    var mes = new Number(req.params.mes);
+    var ano = new Number(req.params.ano);
+    //console.log("Mes: " + mes + " Ano: " + ano);
+
+    const date = new Date(ano + "-" + mes);
+    var primeiroDia = new Date(date.getFullYear(), date.getMonth(), 1);
+    var ultimoDia = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    //console.log("Primeiro dia do mês: " + primeiroDia);
+    //console.log("Último dia do mês: " + ultimoDia);
+
+    const lancamentos = await Lancamentos.findAll({
+        order: [['dataPagamento','ASC']],
+        where: {
+            "dataPagamento": {
+                [Op.between]: [primeiroDia, ultimoDia],
+            }
+        }
+    });
+
+    const valorPagamentos = await Lancamentos.sum('valor', {
+        where: {
+            tipo: '1',
+            "dataPagamento": {
+                [Op.between]: [primeiroDia, ultimoDia],
+            }
+        }
+    });
+
+    const valorRecebido = await Lancamentos.sum('valor', {
+        where: {
+            tipo: '2',
+            "dataPagamento": {
+            
+            
+                [Op.between]: [primeiroDia, ultimoDia],
+            }
+        }
+    });
+
+    const saldo = new Number(valorRecebido) - new Number(valorPagamentos);
+
+    return res.json({
+        erro: false,
+        lancamentos,
+        valorPagamentos,
+        valorRecebido,
+        saldo
+    });
+});
+
+
 app.listen(8080, function(){
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
