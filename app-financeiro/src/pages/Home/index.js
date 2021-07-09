6ab96eb57d0ec06c652478c9fd3e8e586fa20603
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-09 15:18:58 -0300

    Feat: COmunicaça com o backend Configuraçao do Axios Buscar contas por mes

--- app-financeiro/src/pages/Home/index.js
+++ app-financeiro/src/pages/Home/index.js
@@ -1,76 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { Container, ConteudoTitulo, Titulo, ButtomSuccess, BotaoAcao, AnteriorProximo, ButtomPrimary, Table, TextDanger, TextSuccess, Background}
+import moment from 'moment';
+import { Container, ConteudoTitulo, Titulo, ButtomSuccess, BotaoAcao, AnteriorProximo, ButtomPrimary, Table, TextDanger, TextSuccess, AlertDanger, AlertSuccess}
 from '../../styles/custom_adm';
 import  './index.css'
 
+import api from './../../config/api' 
+
 export const Home = () => {
 
-  const [data, setData] = useState([]);
+	const [data, setData] = useState([]);
+	const [saldo, setSaldo] = useState("");
+	const [valorPago, setValorPago] = useState("");
+	const [valorRecebido, setValorRecebido] = useState("");
 
   var dataAtual = new Date();
   var ano = dataAtual.getFullYear();
   var mes = dataAtual.getMonth() + 1;
-  //console.log(mes + "/" + ano);
-
+  
   const [dataView, setDataView] = useState({
       ano,
       mes
   });
+	
+	const [status, setStatus] = useState({
+		type: '',
+		mensagem: ''
+});
 
-  const anterior = async () => {
-    if (dataView.mes === 1) {
-    setDataView({
-        ano: dataView.ano - 1,
-        mes: 12
-    });
-    } else {
-      setDataView({
-        ano: dataView.ano,
-        mes: dataView.mes - 1
-      });
-    }
-  };
+	const anterior = async () => {
+		if (dataView.mes === 1) {
+			ano = dataView.ano - 1;
+			mes = 12;
+			setDataView({
+				ano,
+				mes
+			});
+			listarExtrato(mes, ano);
+		} else {
+			ano = dataView.ano;
+			mes = dataView.mes - 1;
+			setDataView({
+				ano,
+				mes 
+			});
+			listarExtrato(mes, ano);
+		}
+	};
 
-  const proximo = async () => {
-    if (dataView.mes === 12) {
-      setDataView({
-          ano: dataView.ano + 1,
-          mes: 1
-      })
-    } else {
-      setDataView({
-          ano: dataView.ano,
-          mes: dataView.mes + 1
-      })
-    }
-  }
 
-    const listarExtrato = async e => {
-        var valores = [
-            {
-                "id": 3,
-                "nome": "Água",
-                "valor": 347,
-                "tipo": 1,
-                "situacao": "Pago"
-            },
-            {
-                "id": 2,
-                "nome": "Luz",
-                "valor": 347.68,
-                "tipo": 1,
-                "situacao": "Pendente"
-            },
-            {
-                "id": 1,
-                "nome": "Salário",
-                "valor": 1100,
-                "tipo": 2,
-                "situacao": ""
-            }
-        ]
+	const proximo = async () => {
+		if (dataView.mes === 12) {
+			ano = dataView.ano + 1;
+			mes = 1;
+			setDataView({
+				ano,
+				mes
+			});
+			listarExtrato(mes, ano);
+		} else {
+			ano = dataView.ano;
+			mes = dataView.mes + 1;
+			setDataView({
+				ano,
+				mes
+			});
+			listarExtrato(mes, ano);
+		}
+	}
 
-        setData(valores);
+    const listarExtrato = async (mes, ano) => {
+
+		if ((mes === undefined) && (ano === undefined)) {
+				var dataAtual = new Date();
+				ano = dataAtual.getFullYear();
+				mes = dataAtual.getMonth() + 1;
+		} 
+
+		await api.get("/listar/" + mes + "/" + ano)
+			.then((response) => {
+				setData(response.data.lancamentos);
+				setSaldo(response.data.saldo);
+				setValorPago(response.data.valorPagamentos);
+				setValorRecebido(response.data.valorRecebido);
+			}).catch((err) => {
+				if(err.response){
+					setStatus({
+						type: 'erro',
+						mensagem: err.response.data.mensagem
+					});
+				}else{
+					setStatus({
+						type: 'erro',
+						mensagem: 'Erro: Tente mais tarde!'
+					});
+				}
+			});
     }
 
     useEffect(() => {
@@ -78,53 +102,77 @@ export const Home = () => {
     }, []);
 
     return (
-      <Background>
-        <Container>
-            <ConteudoTitulo>
-                <Titulo>Listar Situação Financeira</Titulo>
-                <BotaoAcao>
-                    <ButtomSuccess>Cadastrar</ButtomSuccess>
-                </BotaoAcao>
-            </ConteudoTitulo>
+			<Container>
+				<ConteudoTitulo>
+					<Titulo>Listar Situação Financeira</Titulo>
+					<BotaoAcao>
+						<ButtomSuccess>Cadastrar</ButtomSuccess>
+					</BotaoAcao>
+				</ConteudoTitulo>
 
-            <AnteriorProximo>
-                <ButtomPrimary type="button" onClick={() => anterior()}>Anterior</ButtomPrimary>
-                <span>{dataView.mes + "/" + dataView.ano}</span>
-                <ButtomPrimary type="button" onClick={() => proximo()}>Próximo</ButtomPrimary>
-            </AnteriorProximo>
+				{status.type === 'erro'? <AlertDanger>{status.mensagem}</AlertDanger>: ""}
+				{status.type === 'success'? <AlertSuccess>{status.mensagem}</AlertSuccess>: ""}
 
-            <Table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>Tipo</th>
-                        <th>Situação</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(item => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.nome}</td>
-                            <td>{item.tipo === 1 ? <TextDanger>Pagamento</TextDanger> : <TextSuccess>Recebido</TextSuccess>}</td>
-                            <td>{item.situacao}</td>
-                            <td>{item.valor}</td>
-                        </tr>
-                    ))}
-                </tbody>
-                <tfoot>
-                  <tr>
-                      <td>Total</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td>586.15</td>
-                  </tr>
-                </tfoot>
-            </Table>
-        </Container>
-        </Background>
-    );
+				<AnteriorProximo>
+					<ButtomPrimary type="button" onClick={() => anterior()}>Anterior</ButtomPrimary>
+						<span>{dataView.mes + "/" + dataView.ano}</span>
+					<ButtomPrimary type="button" onClick={() => proximo()}>Próximo</ButtomPrimary>
+				</AnteriorProximo>
+
+				<Table>
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Nome</th>
+							<th>Tipo</th>
+							<th>Situação</th>
+							<th>Data</th>
+							<th>Valor</th>
+						</tr>
+					</thead>
+						<tbody>
+							{data.map(item => (
+								<tr key={item.id}>
+									<td>{item.id}</td>
+									<td>{item.nome}</td>
+									<td>{item.tipo === 1 ? <TextDanger>Pagamento</TextDanger> : <TextSuccess>Recebido</TextSuccess>}</td>
+									<td>
+										{item.situacao === 1 ? <TextSuccess>Pago</TextSuccess> : ""}
+										{item.situacao === 2 ? <TextDanger>Pendente</TextDanger> : ""}
+										{item.situacao === 3 ? <TextSuccess>Recebido</TextSuccess> : ""}
+									</td>
+									<td>{moment(item.dataPagamento).format('DD/MM/YYYY')}</td>
+									<td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.valor)}</td>
+								</tr>
+							))}
+						</tbody>
+					<tfoot>
+						<tr>
+							<td>Saldo</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(saldo)}</td>
+						</tr>
+						<tr>
+							<td>Valor Pago</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorPago)}</td>
+						</tr>
+						<tr>
+							<td>Valor Recebido</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorRecebido)}</td>
+						</tr>
+					</tfoot>
+				</Table>
+		</Container>
+	);
 };
--- app-financeiro/src/styles/custom_adm.js
+++ app-financeiro/src/styles/custom_adm.js
@@ -84,3 +84,20 @@ export const TextDanger = styled.span`
 export const TextSuccess = styled.span`
     color: #198754;
 `;
+
+export const AlertDanger = styled.p`
+    background-color: #f8d7da;
+    color: #842029;
+    margin: 10px 0;
+    border: 1px solid #f5c2c7;
+    border-radius: 4px;
+    padding: 7px;
+`;
+export const AlertSuccess = styled.p`
+    background-color: #d1e7dd;
+    color: #0f5132;
+    margin: 10px 0;
+    border: 1px solid #badbcc;
+    border-radius: 4px;
+    padding: 7px;
+`;
